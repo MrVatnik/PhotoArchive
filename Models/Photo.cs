dcbd1bd4ac05cf47d12b4d69a55c7088a95705ec
Valery Vatnik <zacvars@gmail.com>
2022-08-06 13:01:39 +0300

    Добавьте файлы проекта.

---
+++ Models/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhotoArchive.Models
+{
+    public class Photo
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [DisplayName("Id")]
+        int Id { get; set;}
+        string Name { get; set; }
+        string Pic { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        bool Is_Liked { get; set; }
+    }
+}
