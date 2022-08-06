811c730bf6405aadd73f1a8eb753dd9dbe96e196
Valery Vatnik <zacvars@gmail.com>
2022-08-06 17:27:23 +0300

    Created basic class for later development. Started showing images.

--- Models/Photo.cs
+++ Models/Photo.cs
@@ -9,13 +9,25 @@ namespace PhotoArchive.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         [DisplayName("Id")]
-        int Id { get; set;}
-        string Name { get; set; }
-        string Pic { get; set; }
+        public int id { get; set;}
+
+        public string Name { get; set; }
+
+        public string Pic { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
-        bool Is_Liked { get; set; }
+
+        public bool Is_Liked { get; set; }
+
+        public int Page { get; set; }
+
+        public int Line { get; set; }
+
+        public int Place_In_Line { get; set; }
+
+        public Formats Format { get; set; }
+
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(window).on('load', function () {
+    $('#photoItem img').each(function () { //you need to put this inside the window.onload-function (not document.ready), otherwise the image dimensions won't be available yet
+        if ($(this).width() / $(this).height() >= 1) {
+            $(this).addClass('photo');
+        } else {
+            $(this).addClass('photo_vertical');
+        }
+        $(this).removeClass('temp');
+        $(this).addClass('block');
+    });
+});
+
+$(window).on('load', function () {
+    $('#details img').each(function () { //you need to put this inside the window.onload-function (not document.ready), otherwise the image dimensions won't be available yet
+        if ($(this).width() / $(this).height() >= 1) {
+            $(this).addClass('photoBig');
+        } else {
+            $(this).addClass('photoBig_vertical');
+        }
+        $(this).removeClass('temp');
+        $(this).addClass('block');
+    });
+});
